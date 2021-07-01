f600047da8c5c00e685292589f6910d7bb38de70
Sergey Kufaev <serg.kufaev@gmail.com>
2021-07-02 02:03:13 +0300

    Implement follow an artist.

---
+++ GigHub/Controllers/FollowingsController.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Web.Http;
+using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class FollowingsController : ApiController
+    {
+        readonly ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == dto.FolloweeId))
+                return BadRequest("Following already exists");
+
+            var following = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = dto.FolloweeId
+            };
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
