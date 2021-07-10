a2e8d273d9b67de19bd50f5dd81823db70bf3793
Sergey Kufaev <serg.kufaev@gmail.com>
2021-07-10 05:25:11 +0300

    Implement Notifications.

---
+++ GigHub/Models/Notification.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Notification
+    {
+        public int Id { get; private set; }
+        public DateTime DateTime { get; private set; }
+        public NotificationType Type { get; private set; }
+        public DateTime? OriginalDateTime { get; private set; }
+        public string OriginalVenue { get; private set; }
+
+        [Required]
+        public Gig Gig { get; private set; }
+
+        protected Notification()
+        {
+        }
+
+        Notification(Gig gig, NotificationType type)
+        {
+            if (gig == null)
+                throw new ArgumentNullException(nameof(gig));
+
+            DateTime = DateTime.Now;
+            (Gig, Type) = (gig, type);
+        }
+
+        public static Notification GigCreated(Gig gig)
+        {
+            return new Notification(gig, NotificationType.GigCreated);
+        }
+
+        public static Notification GigUpdated(Gig newGig, string originalVenue, DateTime originalDateTime)
+        {
+            var notification = new Notification(newGig, NotificationType.GigUpdated)
+            {
+                OriginalVenue = originalVenue,
+                OriginalDateTime = originalDateTime
+            };
+
+            return notification;
+        }
+
+        public static Notification GigCanceled(Gig gig)
+        {
+            return new Notification(gig, NotificationType.GigCanceled);
+        }
+    }
+}
