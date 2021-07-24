a8d85d6b6b31abc3d7c5e7cc67559edd07cb2356
Sergey Kufaev <serg.kufaev@gmail.com>
2021-07-25 01:54:20 +0300

    Replace Data Annotations with Fluent API.

---
+++ GigHub/Persistence/EntityConfigurations/UserNotificationConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+using GigHub.Core.Models;
+
+namespace GigHub.Persistence.EntityConfigurations
+{
+    public class UserNotificationConfiguration : EntityTypeConfiguration<UserNotification>
+    {
+        public UserNotificationConfiguration()
+        {
+            HasKey(un => new { un.UserId, un.NotificationId });
+
+            HasRequired(un => un.User)
+                .WithMany(u => u.UserNotifications)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
