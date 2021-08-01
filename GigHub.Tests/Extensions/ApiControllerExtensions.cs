0ee7598207a769dd0d64cf25a5c3a7d701bedb56
Sergey Kufaev <serg.kufaev@gmail.com>
2021-08-01 06:05:07 +0300

    Add unit tests for GigsController API

---
+++ GigHub.Tests/Extensions/ApiControllerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using System.Web.Http;
+
+namespace GigHub.Tests.Extensions
+{
+    public static class ApiControllerExtensions
+    {
+        public static void MockCurrentUser(this ApiController controller, string userId, string username)
+        {
+            var identity = new GenericIdentity(username);
+            identity.AddClaim(new Claim(ClaimTypes.Name, username));
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, userId));
+
+            var principal = new GenericPrincipal(identity, null);
+
+            controller.User = principal;
+        }
+    }
+}
