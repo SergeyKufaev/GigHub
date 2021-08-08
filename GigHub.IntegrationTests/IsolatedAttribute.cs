7bd208ede23bb80f28931725d5b481835f93fe09
Sergey Kufaev <serg.kufaev@gmail.com>
2021-08-08 07:11:17 +0300

    Add custom IsolatedAttribute

---
+++ GigHub.IntegrationTests/IsolatedAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Transactions;
+
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace GigHub.IntegrationTests
+{
+    public sealed class IsolatedAttribute : Attribute, ITestAction
+    {
+        private TransactionScope _transactionScope;
+
+        public ActionTargets Targets => ActionTargets.Test;
+
+        public void BeforeTest(ITest test)
+        {
+            _transactionScope = new TransactionScope();
+        }
+
+        public void AfterTest(ITest test)
+        {
+            _transactionScope.Dispose();
+        }
+    }
+}
